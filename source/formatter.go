893b4fe057474f2ee1a605163cd7df81f8650f71
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-06-05 10:01:33 +0300

    Napisan kod ctenija fajla.

--- source/formatter.go
+++ source/formatter.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	args := os.Args[1:]
+
 	fmt.Println("Ü formatter")
+
+	fmt.Println("Loading file: ", args[0])
+	file_contents := ReadFile(args[0])
+
+	fmt.Println("File contents: ", file_contents)
+}
+
+func ReadFile(s string) string {
+	file, e := os.Open(s)
+	if e != nil {
+		panic(e)
+	}
+
+	stat, e := file.Stat()
+	if e != nil {
+		panic(e)
+	}
+
+	size := stat.Size()
+
+	bytes := make([]byte, size)
+
+	read_size, e := file.Read(bytes)
+	if e != nil {
+		panic(e)
+	}
+	// TODO - read in loop
+	if int64(read_size) != size {
+		panic("Unexpected read size!")
+	}
+
+	file.Close()
+
+	return string(bytes)
 }
