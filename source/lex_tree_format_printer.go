a25da521198320ebd59d9dca22931045b316ee9c
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-06-09 13:44:15 +0300

    Pytajemsä korrektno scitatj dlinu tekuscej stroki.

--- source/lex_tree_format_printer.go
+++ source/lex_tree_format_printer.go
@@ -13,7 +13,9 @@ func PrintLexTreeNodes(nodes LexTreeNodeList, indentation uint, options *Formatt
 		builder.WriteString(options.indentation_sequence)
 	}
 
-	PrintLexTreeNodes_r(nodes, options, &builder, indentation)
+	current_line_width := CountIndentationsSize(indentation, options)
+
+	PrintLexTreeNodes_r(nodes, options, &builder, indentation, current_line_width)
 
 	builder.WriteString(options.line_end_sequence)
 
@@ -28,32 +30,81 @@ func PrintLexTreeNodes_r(
 	nodes LexTreeNodeList,
 	options *FormattingOptions,
 	out *strings.Builder,
-	indentation uint) {
+	indentation uint,
+	current_line_width uint) {
 
-	single_line_text := WriteLexTreeInSingleLine(nodes, options)
+	text_with_further_split := ""
 
-	line_width := CalculateLineWidth(single_line_text, options)
-	for i := uint(0); i < indentation; i++ {
-		for _, c := range options.indentation_sequence {
-			if c == '\t' {
-				line_width += options.tab_size
+	{
+		builder := strings.Builder{}
+
+		w := current_line_width
+
+		for i, node := range nodes {
+
+			if node.sub_elements == nil {
+
+				if i > 0 && WhitespaceIsNeeded(&nodes[i-1].lexem, &node.lexem) {
+					builder.WriteString(" ")
+					w++
+				}
+
+				builder.WriteString(node.lexem.text)
+				w += uint(len(node.lexem.text))
+
 			} else {
-				line_width++
+
+				builder.WriteString(node.lexem.text)
+				w += uint(len(node.lexem.text))
+
+				if len(node.sub_elements) > 0 {
+					builder.WriteString(" ")
+					w++
+				}
+
+				PrintLexTreeNodes_r(node.sub_elements, options, &builder, indentation, w) // TODO - we need to update current_line_width recursively
+
+				if len(node.sub_elements) > 0 {
+					builder.WriteString(" ")
+					w++
+				}
+
+				builder.WriteString(node.trailing_lexem.text)
+				w += uint(len(node.trailing_lexem.text))
 			}
 		}
+
+		text_with_further_split = builder.String()
 	}
 
-	if line_width <= options.max_line_width {
-		out.WriteString(single_line_text)
-		return
+	// Count max line width for further split.
+	max_line_width := uint(0)
+	{
+		w := current_line_width
+
+		for _, c := range text_with_further_split {
+			if c == '\n' { // TODO - use newline sequence from options.
+				max_line_width = max(max_line_width, w)
+				w = 0
+			} else if c == '\t' {
+				w += options.tab_size
+			} else {
+				w++
+			}
+		}
+
+		max_line_width = max(max_line_width, w)
 	}
 
 	text_with_current_split := ""
-	text_with_further_split := ""
 
-	if len(nodes) > 1 {
+	// Perform split only if further split fails to achieve target line width.
+	if max_line_width > options.max_line_width &&
+		len(nodes) > 1 {
 		// Recursively split and print this list, adding newlines in split points.
 		builder := strings.Builder{}
+
+		w := current_line_width
 
 		// Search for the most important lexem type to use it as splitter.
 		// Ignore last node, because splitting at last node has no sense.
@@ -73,7 +124,7 @@ func PrintLexTreeNodes_r(
 
 			if GetLineSplitLexemPriority(&nodes[i].lexem) == max_priority {
 
-				PrintLexTreeNodes_r(nodes[last_i:i+1], options, &builder, next_indentation)
+				PrintLexTreeNodes_r(nodes[last_i:i+1], options, &builder, next_indentation, w)
 				last_i = i + 1
 
 				next_indentation = indentation + 1
@@ -82,47 +133,14 @@ func PrintLexTreeNodes_r(
 				for i := uint(0); i < next_indentation; i++ {
 					builder.WriteString(options.indentation_sequence)
 				}
+				w = CountIndentationsSize(next_indentation, options)
 			}
 		}
 
 		// Process last segment specially.
-		PrintLexTreeNodes_r(nodes[last_i:], options, &builder, next_indentation)
+		PrintLexTreeNodes_r(nodes[last_i:], options, &builder, next_indentation, w)
 
 		text_with_current_split = builder.String()
-	}
-
-	{
-		builder := strings.Builder{}
-
-		for i, node := range nodes {
-
-			if node.sub_elements == nil {
-
-				if i > 0 && WhitespaceIsNeeded(&nodes[i-1].lexem, &node.lexem) {
-					builder.WriteString(" ")
-				}
-
-				builder.WriteString(node.lexem.text)
-
-			} else {
-
-				builder.WriteString(node.lexem.text)
-
-				if len(node.sub_elements) > 0 {
-					builder.WriteString(" ")
-				}
-
-				PrintLexTreeNodes_r(node.sub_elements, options, &builder, indentation)
-
-				if len(node.sub_elements) > 0 {
-					builder.WriteString(" ")
-				}
-
-				builder.WriteString(node.trailing_lexem.text)
-			}
-		}
-
-		text_with_further_split = builder.String()
 	}
 
 	if len(text_with_current_split) == 0 {
@@ -133,35 +151,7 @@ func PrintLexTreeNodes_r(
 		out.WriteString(text_with_current_split)
 	} else {
 
-		// Split result at this level and at further levels gives identical number of lines.
-		// Count max line with for further split and reject it if it gives too long lines.
-
-		max_line_width := uint(0)
-		current_line_width := uint(0)
-
-		for i := uint(0); i < indentation; i++ {
-			for _, c := range options.indentation_sequence {
-				if c == '\t' {
-					current_line_width += options.tab_size
-				} else {
-					current_line_width++
-				}
-			}
-		}
-
-		for _, c := range text_with_further_split {
-			if c == '\n' { // TODO - use newline sequence from options.
-				max_line_width = max(max_line_width, current_line_width)
-				current_line_width = 0
-			} else if c == '\t' {
-				current_line_width += options.tab_size
-			} else {
-				current_line_width++
-			}
-		}
-
-		max_line_width = max(max_line_width, current_line_width)
-
+		// Split result at this level if further level can't achieve target number of lines.
 		if max_line_width > options.max_line_width {
 			out.WriteString(text_with_current_split)
 		} else {
@@ -197,42 +187,17 @@ func CountNewlines(s string) uint {
 	return count
 }
 
-func WriteLexTreeInSingleLine(nodes LexTreeNodeList, options *FormattingOptions) string {
-
-	builder := strings.Builder{}
-
-	WriteLexTreeInSingleLine_r(nodes, &builder)
-
-	return builder.String()
-}
-
-func WriteLexTreeInSingleLine_r(nodes LexTreeNodeList, out *strings.Builder) {
-	for i, node := range nodes {
-
-		if node.sub_elements == nil {
-
-			if i > 0 && WhitespaceIsNeeded(&nodes[i-1].lexem, &node.lexem) {
-				out.WriteString(" ")
-			}
-
-			out.WriteString(node.lexem.text)
+func CountIndentationsSize(indentation uint, options *FormattingOptions) uint {
+	count := uint(0)
+	for _, c := range options.indentation_sequence {
+		if c == '\t' {
+			count += options.tab_size
 		} else {
-
-			out.WriteString(node.lexem.text)
-
-			if len(node.sub_elements) > 0 {
-				out.WriteString(" ")
-			}
-
-			WriteLexTreeInSingleLine_r(node.sub_elements, out)
-
-			if len(node.sub_elements) > 0 {
-				out.WriteString(" ")
-			}
-
-			out.WriteString(node.trailing_lexem.text)
+			count++
 		}
 	}
+
+	return count * indentation
 }
 
 // More priority - more likely to split.
